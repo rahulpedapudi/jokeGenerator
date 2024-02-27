0b6f10d76c06b12490ff11575a8448999e242c5f
Rahul <rahulpedapudi.01@gmail.com>
2024-02-27 22:50:04 +0530

    add fav functionality and some error handling

--- index.js
+++ index.js
@@ -17,35 +17,74 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // ! Light and Dark Modes
 
 var categoryChoice = "";
+var result;
+var fav = [];
 
-app.post("/", async (req, res) => {
-  categoryChoice = req.body["category"];
-  console.log(categoryChoice);
-  const result = await axios.get(API + categoryChoice, {
-    params: {
-      type: "twopart",
-    },
-  });
-  const data = JSON.stringify(result.data);
-  console.log(result.data);
-  res.render("index.ejs", {
-    setup: result.data.setup,
-    delivery: result.data.delivery,
-  });
+// ! add to favourites
+app.post("/fav", (req, res) => {
+  console.log(result.data.id);
+  fav.push(result.data.id);
+  res.redirect("/");
 });
 
+// ! handles the favourite list and renders them
+app.get("/favlist", async (req, res) => {
+  try {
+    var favlist = [];
+
+    for (let index = 0; index < fav.length; index++) {
+      const element = fav[index];
+
+      const favResult = await axios.get(API + `any?idRange=${element}`);
+      const favData = favResult.data;
+      favlist.push({ fsetup: favData.setup, fdelivery: favData.delivery });
+    }
+
+    res.render("fav.ejs", {
+      list: favlist,
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+// ! Shows the Relevant Joke according to category.
+app.post("/", async (req, res) => {
+  try {
+    categoryChoice = req.body["category"];
+    result = await axios.get(API + categoryChoice, {
+      params: {
+        type: "twopart",
+      },
+    });
+    const data = JSON.stringify(result.data);
+    res.render("index.ejs", {
+      setup: result.data.setup,
+      delivery: result.data.delivery,
+      category: result.data.category,
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+// ! Root route - Random Joke
 app.get("/", async (req, res) => {
-  const result = await axios.get(API + "any" + "?safe-mode", {
-    params: {
-      type: "twopart",
-    },
-  });
-  const data = JSON.stringify(result.data);
-  console.log(result.data);
-  res.render("index.ejs", {
-    setup: result.data.setup,
-    delivery: result.data.delivery,
-  });
+  try {
+    result = await axios.get(API + "any" + "?safe-mode", {
+      params: {
+        type: "twopart",
+      },
+    });
+    const data = JSON.stringify(result.data);
+    res.render("index.ejs", {
+      setup: result.data.setup,
+      delivery: result.data.delivery,
+      category: result.data.category,
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
 });
 
 app.listen(PORT, () => {
