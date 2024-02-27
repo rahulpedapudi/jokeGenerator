283b56b40f323f3bb0050e9641a3dbd8b0d50351
Rahul <rahulpedapudi.01@gmail.com>
2024-02-27 19:12:25 +0530

    add reveal punchline and ui improvement

--- index.js
+++ index.js
@@ -10,8 +10,32 @@ const API = "https://v2.jokeapi.dev/joke/";
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// ! Add to Favourites
+// ! Share Jokes
+// ! Infinite Scroll??!?
+// ! Copy to clipboard
+// ! Light and Dark Modes
+
+var categoryChoice = "";
+
+app.post("/", async (req, res) => {
+  categoryChoice = req.body["category"];
+  console.log(categoryChoice);
+  const result = await axios.get(API + categoryChoice, {
+    params: {
+      type: "twopart",
+    },
+  });
+  const data = JSON.stringify(result.data);
+  console.log(result.data);
+  res.render("index.ejs", {
+    setup: result.data.setup,
+    delivery: result.data.delivery,
+  });
+});
+
 app.get("/", async (req, res) => {
-  const result = await axios.get(API + "any", {
+  const result = await axios.get(API + "any" + "?safe-mode", {
     params: {
       type: "twopart",
     },
